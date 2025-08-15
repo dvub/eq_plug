d9ffb56a9b78bf3fc6c552f34df228fad8c2377d
dvub <dvubdevs@gmail.com>
2025-08-15 08:02:46 -0700

    fix embedded gui

--- gui/next.config.ts
+++ gui/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // when we do static export, we cant use image optimization (i guess)
+  images: {
+    unoptimized: true,
+  },
+   distDir: 'assets',
 };
 
 export default nextConfig;
