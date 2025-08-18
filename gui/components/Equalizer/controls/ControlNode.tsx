b79e0b5ec86fe1086d02a9642a688bb77b867200
dvub <dvubdevs@gmail.com>
2025-08-17 23:33:41 -0700

    work on draggable nodes

--- gui/components/Equalizer/controls/ControlNode.tsx
+++ gui/components/Equalizer/controls/ControlNode.tsx
@@ -1,21 +1,46 @@
+import { usePluginListener } from '@/hooks/usePluginListener';
 import { sendToPlugin } from '@/lib';
-import { Parameter } from '@/lib/parameters';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Draggable, { DraggableEvent } from 'react-draggable';
+import { LABEL_MARGIN } from '../Equalizer';
+
+// TODO: fix bounds
+// TODO: fix linear/log scaling mismatch with control
 
 export function EqControls() {
-	console.log('hi i rendered');
-
 	const parentRef = useRef<HTMLDivElement>(null);
+
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+
+	usePluginListener((message) => {
+		if (message.type !== 'initResponse') return;
+
+		const width = parentRef.current!.offsetWidth;
+		const height = parentRef.current!.offsetHeight;
+		let x = 0;
+		let y = 0;
+
+		message.data.initParams.forEach((parameterUpdate) => {
+			if (parameterUpdate.parameterId === 'lowpass_freq') {
+				x = parameterUpdate.value * width;
+			}
+			if (parameterUpdate.parameterId === 'lowpass_q') {
+				y = (1 - parameterUpdate.value) * height;
+				console.log(y);
+			}
+		});
+
+		setPosition({ x, y });
+	});
 
 	const ref = useRef<HTMLDivElement>(null);
 
 	// TODO: should we debounce this in any way?
 	function handleDrag(e: DraggableEvent) {
+		const castEvent = e as React.MouseEvent<HTMLElement>;
+
 		const width = parentRef.current!.offsetWidth;
 		const height = parentRef.current!.offsetHeight;
-
-		const castEvent = e as React.MouseEvent<HTMLElement>;
 
 		const scaledX = castEvent.clientX;
 		const scaledY = height - castEvent.clientY;
@@ -38,28 +63,33 @@ export function EqControls() {
 				value: normalizedY,
 			},
 		});
-	}
 
+		setPosition({ x: castEvent.clientX, y: castEvent.clientY });
+	}
+	const nodeRef = useRef<HTMLElement>({} as any);
 	return (
-		<div className='w-full h-full absolute' ref={parentRef}>
+		<div
+			className='h-full absolute'
+			ref={parentRef}
+			style={{ width: `${600 - LABEL_MARGIN}px` }}
+		>
 			<Draggable
-				nodeRef={ref as unknown as React.RefObject<HTMLElement>}
-				onDrag={handleDrag}
-				bounds={'parent'} // TODO: dont allow this to go into the label margin
+				position={position}
+				nodeRef={nodeRef}
+				onStart={(e) => console.log(e)}
+				// onDrag={handleDrag}
+				bounds={'parent'}
 			>
-				<div className='h-5 w-5  bg-red-500 rounded-lg' ref={ref}>
-					HI
-				</div>
+				<div
+					className='h-5 w-5  bg-red-400 rounded-xl hover:cursor-pointer'
+					ref={nodeRef}
+					onDrag={handleDrag}
+				/>
 			</Draggable>
 		</div>
 	);
 }
 
-function valueToNormalized(value: number, min: number, max: number) {
-	const minl = Math.log2(min);
-	const range = Math.log2(max) - minl;
-	return (Math.log2(value) - minl) / range;
-}
 function clamp(input: number, min: number, max: number) {
 	return Math.min(Math.max(input, min), max);
 }
