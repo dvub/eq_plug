4e12988a53b1f7ad7b5bd51a3ada326ac41a7e04
dvub <dvubdevs@gmail.com>
2025-08-18 18:50:37 -0700

    work on EQ controls (bad right now)

--- gui/components/Equalizer/controls/Control.tsx
+++ gui/components/Equalizer/controls/Control.tsx
@@ -2,11 +2,11 @@ import { Message } from '@/bindings/Message';
 import { usePluginListener } from '@/hooks/usePluginListener';
 import { sendToPlugin } from '@/lib';
 import { Parameter } from '@/lib/parameters';
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 import Draggable, { DraggableEvent } from 'react-draggable';
 
 // TODO: fix control vs. response mismatch
-// fix callback list not properly clearing
+const COMPONENT_SIZE = 20;
 
 export function EqControlNode(props: {
 	containerWidth: number;
@@ -14,68 +14,71 @@ export function EqControlNode(props: {
 	horizontalParam: Parameter;
 	verticalParam: Parameter;
 }) {
-	const COMPONENT_SIZE = 20;
-
 	const { containerWidth, containerHeight, horizontalParam, verticalParam } =
 		props;
 
 	const width = containerWidth - COMPONENT_SIZE;
-	const height = containerHeight - COMPONENT_SIZE;
-
-	const [dragging, setDragging] = useState(false);
-	const [position, setPosition] = useState({ x: 0, y: 0 });
+	const height = containerHeight;
 
 	const nodeRef = useRef<HTMLElement>(null);
 
-	const handlePluginMessage = useCallback(
-		(message: Message) => {
-			if (!dragging && message.type === 'parameterUpdate') {
+	// const [dragging, setDragging] = useState(false);
+
+	const [normalizedPositions, setNormalizedPositions] = useState({
+		x: 0,
+		y: 0,
+	});
+
+	const position = {
+		x: normalizedPositions.x * width,
+		y: normalizedPositions.y * height,
+	};
+
+	useEffect(() => {
+		const unsubscribe = window.plugin.listen((m) => {
+			const message: Message = JSON.parse(m);
+			if (message.type === 'parameterUpdate') {
 				const parameterUpdate = message.data;
 
-				let x = position.x;
-				let y = position.y;
 				if (parameterUpdate.parameterId === horizontalParam) {
-					x = parameterUpdate.value * width;
+					const x = parameterUpdate.value;
+					setNormalizedPositions((prevState) => ({
+						...prevState,
+						x,
+					}));
 				}
 				if (parameterUpdate.parameterId === verticalParam) {
-					y = (1 - parameterUpdate.value) * height;
+					const y = 1 - parameterUpdate.value;
+					setNormalizedPositions((prevState) => ({
+						...prevState,
+						y,
+					}));
 				}
-
-				setPosition({ x, y });
 			}
 
 			if (message.type === 'initResponse') {
-				let x = position.x;
-				let y = position.y;
 				message.data.initParams.forEach((parameterUpdate) => {
 					if (parameterUpdate.parameterId === horizontalParam) {
-						console.log('got init response, width:', width);
-						x = parameterUpdate.value * width;
+						const x = parameterUpdate.value;
+						setNormalizedPositions((prevState) => ({
+							x,
+							y: prevState.y,
+						}));
 					}
 					if (parameterUpdate.parameterId === verticalParam) {
-						y = (1 - parameterUpdate.value) * height;
+						const y = 1 - parameterUpdate.value;
+						setNormalizedPositions((prevState) => ({
+							x: prevState.x,
+							y,
+						}));
 					}
 				});
-
-				setPosition({ x, y });
 			}
-		},
-		[
-			dragging,
-			position.x,
-			position.y,
-			horizontalParam,
-			verticalParam,
-			width,
-			height,
-			containerWidth,
-			containerHeight,
-		]
-	);
-
-	usePluginListener((message) => {
-		handlePluginMessage(message);
-	});
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, [height, horizontalParam, verticalParam, width]);
 
 	// TODO: should we debounce this in any way?
 	function handleDrag(event: DraggableEvent) {
@@ -87,6 +90,7 @@ export function EqControlNode(props: {
 		const normalizedX = clamp(newX / width, 0, 1);
 		const normalizedY = clamp(newY / height, 0, 1);
 
+		/*
 		sendToPlugin({
 			type: 'parameterUpdate',
 			data: {
@@ -100,22 +104,15 @@ export function EqControlNode(props: {
 				parameterId: verticalParam,
 				value: normalizedY,
 			},
-		});
+		});*/
 
 		const x = clamp(e.clientX, 0, width);
 		const y = clamp(e.clientY, 0, height);
-		setPosition({ x, y });
+		// setPosition({ x, y });
 	}
 
 	return (
-		<Draggable
-			position={position}
-			nodeRef={nodeRef}
-			bounds={'parent'}
-			onDrag={handleDrag}
-			onStop={() => setDragging(false)}
-			onStart={() => setDragging(true)}
-		>
+		<Draggable position={position} nodeRef={nodeRef} bounds={'parent'}>
 			<div className='h-5 w-5  bg-red-400 rounded-xl' ref={nodeRef} />
 		</Draggable>
 	);
@@ -125,9 +122,11 @@ function clamp(input: number, min: number, max: number) {
 	return Math.min(Math.max(input, min), max);
 }
 
+/*
 function norm(plain: number, min: number, max: number, factor: number) {
 	return Math.pow(
 		(Math.min(Math.max(plain, min), max) - min) / (max - min),
 		factor
 	);
 }
+*/
